2c5df0f8c2782e0b803b439ccd64a654595bc9b2
Rick Sun <liamcloud@protonmail.com>
2023-03-17 16:15:44 +0100

    flex

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
     <footer className="text-center h-16 sm:h-20 w-full sm:pt-2 pt-4 border-t mt-5 flex sm:flex-row flex-col justify-between items-center px-3 space-y-3 sm:mb-0 mb-3 border-gray-500">
       <div className="text-gray-500">
         <a
-          href="https://replicate.com/"
+          href=""
           target="_blank"
           rel="noreferrer"
           className="font-bold hover:underline transition hover:text-gray-300 underline-offset-2"
@@ -17,7 +17,7 @@ export default function Footer() {
       </div>
       <div className="flex space-x-4 pb-4 sm:pb-0">
         <Link
-          href="https://twitter.com/nutlope"
+          href="https://instagram.com/architech"
           className="group"
           aria-label="TaxPal on Twitter"
         >
