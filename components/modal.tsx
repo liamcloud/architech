a511168edd83d0121b3c4df7b1d2aa5d2858ce8b
Rick Sun <liamcloud@protonmail.com>
2023-03-23 12:35:45 +0100

    touch

--- components/modal.tsx
+++ components/modal.tsx
@@ -5,10 +5,10 @@ import Link from "next/link";
 import { AiOutlineCheckCircle } from "react-icons/ai";
 import { createCheckoutSession } from "../stripe/createCheckoutSession";
 import { createCheckout } from "../stripe/createCheckout29";
-import { useAuthState } from "react-firebase-hooks/auth"
-import firebase from "../firebase/clientApp"
+import { useAuthState } from "react-firebase-hooks/auth";
+import firebase from "../firebase/clientApp";
 import usePremiumStatus from "../stripe/usePremiumStatus";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 interface ModalProps {
@@ -16,18 +16,25 @@ interface ModalProps {
   onClose: () => void;
 }
 
-
 const Modal = ({ open, onClose }: ModalProps) => {
   if (!open) {
     return null;
   }
-  const [user, userLoading] = useAuthState((firebase as any).auth())
-  const router = useRouter()
-  const [loading, setLoading] = useState(false)
-  const [loading1, setLoading1] = useState(false)
+  const [user, userLoading] = useAuthState((firebase as any).auth());
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const [loading1, setLoading1] = useState(false);
+  const handleButtonClick = () => {
+    if (user) {
+      setLoading(true);
+      createCheckoutSession(user.uid, setLoading);
+    } else {
+      router.push("/dream");
+      onClose();
+    }
+  };
 
   return (
-    
     <div className="modal" onClick={onClose}>
       <div
         className="element"
@@ -35,7 +42,6 @@ const Modal = ({ open, onClose }: ModalProps) => {
           e.stopPropagation();
         }}
       >
-        
         <h2
           onClick={onClose}
           className="sm:text-3xl text-xl font-bold ml-2 tracking-tight text-right mr-5 cursor-pointer"
@@ -62,22 +68,23 @@ const Modal = ({ open, onClose }: ModalProps) => {
                 per<br></br>month
               </p>
             </div>
-            <div className="h2" onClick={() => {
-              
-    if (user) {
-      setLoading1(true)
-      createCheckout(user.uid, setLoading1);
-    }else{
-      router.push('/dream');
-      onClose()
-    }
-  }}
->
-{loading1 ? (
-  <div className="spinner"></div>
-) : (
-  <h2 className="sub">Subscribe</h2>
-)}
+            <div
+              className="h2"
+              onClick={() => {
+                if (user) {
+                  setLoading1(true);
+                  createCheckout(user.uid, setLoading1);
+                } else {
+                  router.push("/dream");
+                  onClose();
+                }
+              }}
+            >
+              {loading1 ? (
+                <div className="spinner"></div>
+              ) : (
+                <h2 className="sub">Subscribe</h2>
+              )}
             </div>
             <ul className="list">
               <li className="test flex">
@@ -130,24 +137,20 @@ const Modal = ({ open, onClose }: ModalProps) => {
               </p>
             </div>
 
-            <div className="h2" onClick={() => {
-    if (user) {
-      setLoading(true)
-      createCheckoutSession(user.uid, setLoading);
-    }else{
-      router.push('/dream');
-      onClose()
-    }
+            <div
+  className="h2"
+  onClick={handleButtonClick}
+  onTouchEnd={(e) => {
+    e.preventDefault(); // To prevent triggering both touch and click events.
+    handleButtonClick();
   }}
 >
-{loading ? (
-  <div className="spinner"></div>
-) : (
-  <h2 className="sub">Subscribe</h2>
-)}
-
-            </div>
-
+  {loading ? (
+    <div className="spinner"></div>
+  ) : (
+    <h2 className="sub">Subscribe</h2>
+  )}
+</div>
             <ul className="list">
               <li className="test flex">
                 <AiOutlineCheckCircle className="gello" />
